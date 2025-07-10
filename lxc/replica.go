2b0ce980a819a71f5141ce22f9ff97d1e6ece7bc
Kadin Sayani <kadin.sayani@canonical.com>
2025-07-10 15:24:52 -0600

    lxc/replica: Add `replica get/set/unset` commands
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxc/replica.go
+++ lxc/replica.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"maps"
 	"os"
 	"sort"
 	"strings"
@@ -44,6 +46,18 @@ func (c *cmdReplica) command() *cobra.Command {
 	// List.
 	replicaListCmd := cmdReplicaList{global: c.global}
 	cmd.AddCommand(replicaListCmd.command())
+
+	// Get.
+	replicaGetCmd := cmdReplicaGet{global: c.global}
+	cmd.AddCommand(replicaGetCmd.command())
+
+	// Set.
+	replicaSetCmd := cmdReplicaSet{global: c.global}
+	cmd.AddCommand(replicaSetCmd.command())
+
+	// Unset.
+	replicaUnsetCmd := cmdReplicaUnset{global: c.global, replicaSet: &replicaSetCmd}
+	cmd.AddCommand(replicaUnsetCmd.command())
 
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 	cmd.Args = cobra.NoArgs
@@ -342,4 +356,214 @@ func (c *cmdReplicaList) run(cmd *cobra.Command, args []string) error {
 	}
 
 	return cli.RenderTable(c.flagFormat, header, data, replicas)
+}
+
+// Get.
+type cmdReplicaGet struct {
+	global *cmdGlobal
+
+	flagIsProperty bool
+}
+
+func (c *cmdReplicaGet) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("get", i18n.G("[<remote>:]<replica> <key>"))
+	cmd.Short = i18n.G("Get values for replica configuration keys")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Get values for replica configuration keys`))
+
+	cmd.RunE = c.run
+
+	cmd.Flags().BoolVarP(&c.flagIsProperty, "property", "p", false, i18n.G("Get the key as a replica property"))
+
+	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) == 0 {
+			return c.global.cmpTopLevelResource("replica", toComplete)
+		}
+
+		if len(args) == 1 {
+			return c.global.cmpReplicaConfig(args[0])
+		}
+
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	return cmd
+}
+
+func (c *cmdReplicaGet) run(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := c.global.CheckArgs(cmd, args, 2, 2)
+	if exit {
+		return err
+	}
+
+	// Parse remote
+	resources, err := c.global.ParseServers(args[0])
+	if err != nil {
+		return err
+	}
+
+	resource := resources[0]
+
+	if resource.name == "" {
+		return errors.New(i18n.G("Missing replica name"))
+	}
+
+	// Get the configuration key
+	replica, _, err := resource.server.GetReplica(resource.name)
+	if err != nil {
+		return err
+	}
+
+	if c.flagIsProperty {
+		w := replica.Writable
+		res, err := getFieldByJSONTag(&w, args[1])
+		if err != nil {
+			return fmt.Errorf(i18n.G("The property %q does not exist on the replica %q: %v"), args[1], resource.name, err)
+		}
+
+		fmt.Printf("%v\n", res)
+	} else {
+		fmt.Printf("%s\n", replica.Config[args[1]])
+	}
+
+	return nil
+}
+
+// Set.
+type cmdReplicaSet struct {
+	global *cmdGlobal
+
+	flagIsProperty bool
+}
+
+func (c *cmdReplicaSet) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("set", i18n.G("[<remote>:]<replica> <key>=<value>..."))
+	cmd.Short = i18n.G("Set replica configuration keys")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Set replica configuration keys
+
+For backward compatibility, a single configuration key may still be set with
+lxc replica set [<remote>:]<replica> <key> <value>`))
+
+	cmd.RunE = c.run
+
+	cmd.Flags().BoolVarP(&c.flagIsProperty, "property", "p", false, i18n.G("Set the key as a replica property"))
+
+	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) == 0 {
+			return c.global.cmpTopLevelResource("replica", toComplete)
+		}
+
+		if len(args) == 1 {
+			return c.global.cmpReplicaConfig(args[0])
+		}
+
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	return cmd
+}
+
+func (c *cmdReplicaSet) run(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := c.global.CheckArgs(cmd, args, 3, -1)
+	if exit {
+		return err
+	}
+
+	// Parse remote
+	resources, err := c.global.ParseServers(args[0])
+	if err != nil {
+		return err
+	}
+
+	resource := resources[0]
+
+	if resource.name == "" {
+		return errors.New(i18n.G("Missing replica name"))
+	}
+
+	// Get the replica
+	replica, etag, err := resource.server.GetReplica(resource.name)
+	if err != nil {
+		return err
+	}
+
+	// Set the configuration key
+	keys, err := getConfig(args[1:]...)
+	if err != nil {
+		return err
+	}
+
+	writable := replica.Writable()
+	if c.flagIsProperty {
+		if cmd.Name() == "unset" {
+			for k := range keys {
+				err := unsetFieldByJSONTag(&writable, k)
+				if err != nil {
+					return fmt.Errorf(i18n.G("Error unsetting property: %v"), err)
+				}
+			}
+		} else {
+			err := unpackKVToWritable(&writable, keys)
+			if err != nil {
+				return fmt.Errorf(i18n.G("Error setting properties: %v"), err)
+			}
+		}
+	} else {
+		maps.Copy(writable.Config, keys)
+	}
+
+	return resource.server.UpdateReplica(resource.name, writable, etag)
+}
+
+// Unset.
+type cmdReplicaUnset struct {
+	global     *cmdGlobal
+	replicaSet *cmdReplicaSet
+
+	flagIsProperty bool
+}
+
+func (c *cmdReplicaUnset) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("unset", i18n.G("[<remote>:]<link> <key>"))
+	cmd.Short = i18n.G("Unset replica configuration keys")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Unset replica configuration keys`))
+
+	cmd.RunE = c.run
+
+	cmd.Flags().BoolVarP(&c.flagIsProperty, "property", "p", false, i18n.G("Unset the key as a replica property"))
+
+	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) == 0 {
+			return c.global.cmpTopLevelResource("replica", toComplete)
+		}
+
+		if len(args) == 1 {
+			return c.global.cmpReplicaConfig(args[0])
+		}
+
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	return cmd
+}
+
+func (c *cmdReplicaUnset) run(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := c.global.CheckArgs(cmd, args, 3, 3)
+	if exit {
+		return err
+	}
+
+	c.replicaSet.flagIsProperty = c.flagIsProperty
+
+	// Get the current cluster link.
+	args = append(args, "")
+	return c.replicaSet.run(cmd, args)
 }
